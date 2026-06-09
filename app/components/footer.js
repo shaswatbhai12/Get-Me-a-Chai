190a85541835e5d3231e147eb3a2aa8a3d082b16
shaswat <shaswatkumar9868@gmail.com>
2026-06-09 14:26:29 +0530

    Just Created the project and added some things

---
+++ app/components/footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+    <footer className="bg-black text-white flex items-center justify-center px-4 h-16">
+        <p className="text-center">Copyright &copy; Get me a chai - All Rights Reserved</p>
+    </footer>
+)}
+
+export default Footer
