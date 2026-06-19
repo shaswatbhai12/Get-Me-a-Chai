e921ced3cd3a6c3202a597c51e6b8a88e8a2780f
shaswat <shaswatkumar9868@gmail.com>
2026-06-19 20:55:37 +0530

    Developing the User Page

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,6 +1,17 @@
+"use client"
 import React from 'react'
+import { useSession, signIn, signOut } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 
 const Dashboard = () => {
+    const { data: session } = useSession()
+
+    const router = useRouter()
+    if(!session) {
+        setTimeout(() => {
+            router.push('/login')
+        }, 0);
+    }
   return (
     <div>
       dashboard
