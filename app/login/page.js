d22a3f5dd6821f6ab644909f51572df18e6218ec
shaswat <shaswatkumar9868@gmail.com>
2026-06-28 14:52:00 +0530

    Added Coming soon to Apple

--- app/login/page.js
+++ app/login/page.js
@@ -118,7 +118,7 @@ const Login = () => {
     </svg>
     <span>Continue with Github</span>
         </button>
-        <button 
+        <button dissabled={true}
         className="flex items-center w-64  text-black bg-slate-50 border border-gray-300 rounded-lg shadow-md max-w-xs px-6 py-2 text-sm font-medium  hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
             <svg className="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
         viewBox="-1.5 0 20 20" version="1.1">
@@ -134,7 +134,7 @@ const Login = () => {
             </g>
         </g>
     </svg>
-    <span>Continue with Apple</span>
+    <span>Continue with Apple (Coming Soon)</span>
         </button>
       </div>
     </div>
