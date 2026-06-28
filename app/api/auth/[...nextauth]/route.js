4ea5fdb12d822d6bdb45742f3263d7e28bc2e13e
shaswat <shaswatkumar9868@gmail.com>
2026-06-28 10:59:47 +0530

    added Facebook Auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 // import Apple from "next-auth/providers/apple";
 import Google from "next-auth/providers/google";
-// import Facebook from "next-auth/providers/facebook";
+import Facebook from "next-auth/providers/facebook";
 // import Email from "next-auth/providers/email";
 import Github from "next-auth/providers/github";
 import mongoose from "mongoose";
@@ -19,10 +19,10 @@ export const authoptions = {
         //     clientId: process.env.APPLE_ID,
         //     clientSecret: process.env.APPLE_SECRET
         // }),
-        // Facebook({
-        //     clientId: process.env.FACEBOOK_ID,
-        //     clientSecret: process.env.FACEBOOK_SECRET
-        // }),
+        Facebook({
+            clientId: process.env.FACEBOOK_ID,
+            clientSecret: process.env.FACEBOOK_SECRET
+        }),
         Google({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET
@@ -34,7 +34,7 @@ export const authoptions = {
     ],
     callbacks: {
         async signIn({ user, account, profile }) {
-    if (account.provider === "github" || account.provider === "google") {
+    if (account.provider === "github" || account.provider === "google" || account.provider === "facebook") {
         await connectDB();
 
         const targetEmail =
--- app/login/page.js
+++ app/login/page.js
@@ -80,7 +80,7 @@ const Login = () => {
     </svg>
     <span>Continue with Twitter</span>
         </button>
-        <button 
+        <button onClick={()=>{signIn("facebook")}}
         className="flex items-center w-64  text-black bg-slate-50 border border-gray-300 rounded-lg shadow-md max-w-xs px-6 py-2 text-sm font-medium  hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
                 <svg className="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
         viewBox="0 0 48 48" version="1.1">
