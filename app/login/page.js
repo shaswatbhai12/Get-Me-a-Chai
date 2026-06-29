6b8fb3ddd5168cec9f3bd2517cd1b8afffe54161
shaswat <shaswatkumar9868@gmail.com>
2026-06-29 09:10:23 +0530

    Changed name of Twitter to X

--- app/login/page.js
+++ app/login/page.js
@@ -78,7 +78,7 @@ const Login = () => {
             </g>
         </g>
     </svg>
-    <span>Continue with Twitter</span>
+    <span>Continue with X</span>
         </button>
         <button onClick={()=>{signIn("facebook")}}
         className="flex items-center w-64  text-black bg-slate-50 border border-gray-300 rounded-lg shadow-md max-w-xs px-6 py-2 text-sm font-medium  hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
