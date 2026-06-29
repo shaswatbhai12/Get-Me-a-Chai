8772b7e13ab5032d1f6e25ecf6cd0d31805ed135
shaswat <shaswatkumar9868@gmail.com>
2026-06-29 09:22:36 +0530

    LinkedIn Coming Soon

--- app/login/page.js
+++ app/login/page.js
@@ -48,7 +48,7 @@ const Login = () => {
     </svg>
     <span>Continue with google</span>
         </button>
-        <button 
+        <button dissabled={true}
         className="flex items-center w-64  text-black bg-slate-50 border border-gray-300 rounded-lg shadow-md max-w-xs px-6 py-2 text-sm font-medium  hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
             <svg className="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
         viewBox="0 -2 44 44" version="1.1">
@@ -62,7 +62,7 @@ const Login = () => {
             </g>
         </g>
     </svg>
-    <span>Continue with LinkedIn</span>
+    <span>Continue with LinkedIn (Coming Soon)</span>
         </button>
         <button onClick={()=>{signIn("twitter")}}
         className="flex items-center w-64  text-black bg-slate-50 border border-gray-300 rounded-lg shadow-md max-w-xs px-6 py-2 text-sm font-medium  hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
